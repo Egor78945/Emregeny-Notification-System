e5cc59fe764f0c28004021cc0fcc794e9b3c45c4
egor <Egor78396@gmail.com>
2024-06-13 18:52:49 +0300

    ENS(base package)

---
+++ ENS/src/main/java/org/ens/app/App.java
@@ -0,0 +1,13 @@
+package org.ens.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
