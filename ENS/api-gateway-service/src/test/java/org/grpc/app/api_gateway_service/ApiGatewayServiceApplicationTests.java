4272f3ea36fc1b3da16bff72058fdc3f923835e1
egor <Egor78396@gmail.com>
2024-06-14 18:36:28 +0300

    ENS(api-gateway-service)

---
+++ ENS/api-gateway-service/src/test/java/org/grpc/app/api_gateway_service/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.grpc.app.api_gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
