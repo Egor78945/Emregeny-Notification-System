1a6afe88f94f63a7e6bcd04a7f58cacc35025de0
egor <Egor78396@gmail.com>
2024-06-14 20:28:43 +0300

    ENS(api-user-service base)

---
+++ ENS/user-api-service/src/test/java/org/user_api_service/app/UserApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.user_api_service.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
